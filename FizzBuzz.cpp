aec7a980bc9feae36bd974555327d2d296be5bb5
Alejandro Lopez <alejjx@gmail.com>
2014-04-02 15:56:59 -0700

    fixed issue?

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -2,13 +2,13 @@
  
 int main(int argc, char *argv[]) {
     for(unsigned int i = 0; i < 100; i++) {
-<<<<<<< HEAD
+
         if(i % 3 == 0)
             std::cout << i << ": Fizz" << std::endl;
-=======
-		if( i % 5 == 0)
-			std::cout << i << ": " << "Buzz" << std::endl;
->>>>>>> 2f0eacc154f6a686b92bff855450a5bea4e231d4
+        else if( i % 5 == 0)
+		        std::cout << i << ": " << "Buzz" << std::endl;
+        else
+            std::cout << i << std::endl;
     }
     
     return(0);
